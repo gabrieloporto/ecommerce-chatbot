119af21fbfed5f3b7133b9c380f08bf6ecf7d99e
Gabriel Oporto <gabyoporto@gmail.com>
2025-04-17 13:37:22 -0300

    Fix TypeScript error in product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,8 +1,28 @@
 import { products } from "@/app/data/products";
 import { notFound } from "next/navigation";
-import ProductDetail from "@/app/components/ProductDetailt";
+import ProductDetail from "@/app/components/ProductDetail";
+import { Metadata } from "next";
 
-export default function Page({ params }: { params: { id: string } }) {
+type Props = {
+  params: { id: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const product = products.find((p) => p.id === parseInt(params.id));
+
+  if (!product) {
+    return {
+      title: "Producto no encontrado",
+    };
+  }
+
+  return {
+    title: product.name,
+    description: product.description,
+  };
+}
+
+export default function Page({ params }: Props) {
   const product = products.find((p) => p.id === parseInt(params.id));
 
   if (!product) return notFound();
