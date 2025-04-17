50b0dfcf581804fb71410ba75c0e4376c159b36b
Gabriel Oporto <gabyoporto@gmail.com>
2025-04-17 13:29:17 -0300

    fix product details route

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -2,11 +2,7 @@ import { products } from "@/app/data/products";
 import { notFound } from "next/navigation";
 import ProductDetail from "@/app/components/ProductDetailt";
 
-interface Props {
-  params: { id: string };
-}
-
-export default function Page({ params }: Props) {
+export default function Page({ params }: { params: { id: string } }) {
   const product = products.find((p) => p.id === parseInt(params.id));
 
   if (!product) return notFound();
